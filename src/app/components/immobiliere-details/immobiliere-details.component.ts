d1e971fa890a751d728c99fa6a6332e99271be11
anaschaarii <anas.chaari@esprit.tn>
2023-06-28 14:21:41 +0100

    user details update

--- src/app/components/immobiliere-details/immobiliere-details.component.ts
+++ src/app/components/immobiliere-details/immobiliere-details.component.ts
@@ -12,7 +12,8 @@ export class ImmobiliereDetailsComponent {
   {idAttachment:2,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",name:"",idImmobilier:1},
   {idAttachment:3,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",name:"",idImmobilier:1},
   {idAttachment:4,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",name:"",idImmobilier:1},
-  {idAttachment:5,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",name:"",idImmobilier:1}
+  {idAttachment:5,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",name:"",idImmobilier:1},
+  {idAttachment:6,path:"https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",name:"",idImmobilier:1}
 ]
 selectedAttachment:any[]=[]
 selectedImage:string=this.attachmentList[0].path;
